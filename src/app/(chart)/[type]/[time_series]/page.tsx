f1e4c9cf24b14ead192429b7b9b1b7373b5ed241
tchassijordan <tchassijordan@gmail.com>
2024-02-07 22:49:57 +0100

    feat: add more data to visualize and extend autocomplete search bar to recognize them

--- src/app/(chart)/[type]/[time_series]/page.tsx
+++ src/app/(chart)/[type]/[time_series]/page.tsx
@@ -4,6 +4,10 @@ import { Typography, Box } from "@mui/material";
 import Chart from "@/app/components/Chart";
 import dailyMoneyMarketFundFlow from "@/data/daily_money_market_fund_flows.json";
 import capeOfGoodHopeDaily from "@/data/cape-of-good-hope-daily.json";
+import turkeyBalanceOfPayment from "@/data/turkey-balance-of-payments_(Rolling_1yr_Window).json";
+import southKoreaBalanceOfPayment from "@/data/South_Korea_Balance_of_Payments_(Rolling_1yr_Window).json";
+import japanBalanceOfPayment from "@/data/japan-balance-of-payments-(rolling-1yr-Window).json";
+import globalFlowsToEquityAndFixedIncome from "@/data/Global_Flows_to_Equity_and_Fixed_Income.json";
 import { useParams } from "next/navigation";
 
 const dataMap = {
@@ -15,11 +19,31 @@ const dataMap = {
     data: dailyMoneyMarketFundFlow,
     xAxisKey: "SERIES",
   },
+  "turkey-balance-of-payment": {
+    data: turkeyBalanceOfPayment,
+    xAxisKey: "SERIES",
+  },
+  "south-korea-balance-of-payment": {
+    data: southKoreaBalanceOfPayment,
+    xAxisKey: "SERIES",
+  },
+  "japan-balance-of-payment": {
+    data: japanBalanceOfPayment,
+    xAxisKey: "SERIES",
+  },
+  "global-flows-to-equity": {
+    data: globalFlowsToEquityAndFixedIncome,
+    xAxisKey: "SERIES",
+  },
 };
 
 const titleMap = {
   "cape-of-good-hope-daily": "Cape of good hope daily",
   "daily-money-market-fund-flows": "Daily money market fund flows",
+  "turkey-balance-of-payment": "Turkey balance of payment",
+  "south-korea-balance-of-payment": "South korea balance of payment",
+  "japan-balance-of-payment": "Japan balance of payment",
+  "global-flows-to-equity": "Global flows to equity and fixed income",
 };
 
 export default function Home() {
@@ -38,14 +62,7 @@ export default function Home() {
             .replaceAll("%7C", "|")
             .replaceAll("%3A", ":"),
         ]}
-        data={dataMap[type as keyof typeof dataMap].data.filter((item) => {
-          const seriesDate = ((item as any)?.SERIES as any)
-            ? new Date((item as any)?.SERIES as any)
-            : new Date((item as any)?.DateTime as any);
-          const today = new Date();
-
-          return seriesDate.getFullYear() == today.getFullYear();
-        })}
+        data={dataMap[type as keyof typeof dataMap].data}
       />
     </Box>
   );
--- src/app/components/layout/navbar.tsx
+++ src/app/components/layout/navbar.tsx
@@ -61,6 +61,7 @@ export default function Navbar() {
 }
 
 const filters = [
+  // Cape of good hope dialy
   {
     label: "Number of Cargo Ships",
     categorie: "Cape of good hope daily",
@@ -81,6 +82,8 @@ const filters = [
     categorie: "Cape of good hope daily",
     href: "/cape-of-good-hope-daily/Prior Year: 7-day Moving Average",
   },
+
+  // US Money Market
   {
     label: "US Money Market Fund Flows",
     categorie: "Daily money market fund flows",
@@ -90,5 +93,128 @@ const filters = [
     label: "US Money Market Fund Flows (5d m/a)",
     categorie: "Daily money market fund flows",
     href: "/daily-money-market-fund-flows/US Money Market Fund Flows (5d m|a)",
+  },
+
+  // South Korea BoP
+  {
+    label: "Capital Account",
+    categorie: "South Korea balance of payments",
+    href: "/south-korea-balance-of-payment/Capital Account",
+  },
+  {
+    label: "Current Account",
+    categorie: "South Korea balance of payments",
+    href: "/south-korea-balance-of-payment/Current Account",
+  },
+  {
+    label: "Errors and Omissions",
+    categorie: "South Korea balance of payments",
+    href: "/south-korea-balance-of-payment/Errors and Omissions",
+  },
+  {
+    label: "Direct Investment",
+    categorie: "South Korea balance of payments",
+    href: "/south-korea-balance-of-payment/Direct Investment",
+  },
+  {
+    label: "Financial Derivatives",
+    categorie: "South Korea balance of payments",
+    href: "/south-korea-balance-of-payment/Financial Derivatives",
+  },
+  {
+    label: "Other Investment",
+    categorie: "South Korea balance of payments",
+    href: "/south-korea-balance-of-payment/Other Investment",
+  },
+  {
+    label: "Debt Securities",
+    categorie: "South Korea balance of payments",
+    href: "/south-korea-balance-of-payment/Debt Securities",
+  },
+  {
+    label: "Equity",
+    categorie: "South Korea balance of payments",
+    href: "/south-korea-balance-of-payment/Equity",
+  },
+  {
+    label: "Reserves",
+    categorie: "South Korea balance of payments",
+    href: "/south-korea-balance-of-payment/Reserves",
+  },
+
+  // Turkey BoP
+  {
+    label: "Capital Account",
+    categorie: "Turkey balance of payments",
+    href: "/turkey-balance-of-payment/Capital Account",
+  },
+  {
+    label: "Current Account",
+    categorie: "Turkey balance of payments",
+    href: "/turkey-balance-of-payment/Current Account",
+  },
+  {
+    label: "Errors and Omissions",
+    categorie: "Turkey balance of payments",
+    href: "/turkey-balance-of-payment/Errors and Omissions",
+  },
+  {
+    label: "Direct Investment",
+    categorie: "Turkey balance of payments",
+    href: "/turkey-balance-of-payment/Direct Investment",
+  },
+  {
+    label: "Other Investment",
+    categorie: "Turkey balance of payments",
+    href: "/turkey-balance-of-payment/Other Investment",
+  },
+  {
+    label: "Reserves",
+    categorie: "Turkey balance of payments",
+    href: "/turkey-balance-of-payment/Reserves",
+  },
+
+  // Japan BoP
+  {
+    label: "Capital Account",
+    categorie: "Japan balance of payments",
+    href: "/japan-balance-of-payment/Capital Account",
+  },
+  {
+    label: "Current Account",
+    categorie: "Japan balance of payments",
+    href: "/japan-balance-of-payment/Current Account",
+  },
+  {
+    label: "Errors and Omissions",
+    categorie: "Japan balance of payments",
+    href: "/japan-balance-of-payment/Errors and Omissions",
+  },
+  {
+    label: "Direct Investment",
+    categorie: "Japan balance of payments",
+    href: "/japan-balance-of-payment/Direct Investment",
+  },
+  {
+    label: "Other Investment",
+    categorie: "Japan balance of payments",
+    href: "/japan-balance-of-payment/Other Investment",
+  },
+  {
+    label: "Reserves",
+    categorie: "Japan balance of payments",
+    href: "/japan-balance-of-payment/Reserves",
+  },
+
+  // Global Flows to equity
+  {
+    label: "Equity",
+    categorie: "Global flows to equity and fixed income",
+    href: "/global-flows-to-equity/Equity",
+  },
+  {
+    label: "Debt",
+    categorie: "Global flows to equity and fixed income",
+    href: "/global-flows-to-equity/Debt",
   },
 ];
